b46a52995c3ba62ea1842c4ae768777ee3a65081
EifelDriver <eifel.driver@googlemail.com>
2019-01-17 08:20:42 +0100

    Implement the loading / start process

--- dim-tools.js
+++ dim-tools.js
@@ -1,9 +1,13 @@
 /**
  * define some vars
  */
-var this_debug      = 1;
-var this_version    = '0.1';
-var version_file    = 'https://raw.githubusercontent.com/eifeldriver/dim-tools/master/version';
+var this_debug          = 1;
+var this_version        = '0.1';
+var version_file        = 'https://raw.githubusercontent.com/eifeldriver/dim-tools/master/version';
+var loading_starts      = 0;
+var loading_ends        = 0;
+var selector_spinner    = '.dim-loading';
+var watcher             = null;
 
 
 /**
@@ -55,13 +59,68 @@ function insertCss() {
     document.querySelector('head').appendChild(style);
 }
 
-/**
- * init the script
- */
-function initDimTools() {
+//------------------------------------------------------------
 
+function addFactionItemCount() {
+    if (location.href.endsWith('/vendors')) {
+        var vendors = document.querySelectorAll('.vendor-char-items');
+        for (var idx=0; idx<vendors.length; idx++) {
+            var faction = vendors[idx];
+            var cnt     = faction.querySelector('.item-faction').innerText;
+            _debug(faction.querySelector('span > span > span').innerText + ': ' + cnt)
+        }
+    }
 }
 
+
+/**
+ * simple check for running loading process
+ */
+function isStillLoading() {
+    // DOM element only exists on running reload
+    return document.querySelectorAll(selector_spinner).length;
+}
+
+function waitForReloadStarts() {
+    if (isStillLoading()) {
+        loading_starts = 1;
+        window.clearInterval(watcher);
+        _debug("watcher cleared");
+        watcher = window.setInterval(waitForReloadFinished, 1000);
+        _debug("watcher 'wait for loading finished' started");
+        
+    }
+}
+
+function waitForReloadFinished() {
+    if (!isStillLoading()) {
+        loading_ends = 1;
+        window.clearInterval(watcher);
+        _debug("watcher cleared");
+        startDimTools();
+    }
+}
+
+/**
+ * init the script
+ *
+ * 1st = waiting for loading
+ * 2nd = wait for loading finished
+ * 3rd = exec dim-tools
+ */
+function initDimTools() {
+    window.clearInterval(watcher);
+    _debug("watcher cleared");
+    watcher = window.setInterval(waitForReloadStarts, 1000);
+    _debug("watcher 'wait for loading starts' started");
+}
+
+/**
+ * add the tools to the page
+ */
+function startDimTools() {
+    _debug('DIM Tools started');
+}
 
 
 //###########################################################
